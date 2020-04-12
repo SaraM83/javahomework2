3894aad447a7c418ada3f143bbc3e89fc8d9fe03
NannyMika <sara.majava83@gmail.com>
2020-04-12 16:41:43 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Task number 1. Write a calculator. 
 
-/*let number1 = +prompt('Write any number');
+let number1 = +prompt('Write any number');
 let number2 = +prompt('Write any number again');
 let symbol = prompt('Write any of the following: + - * /');
 
@@ -17,17 +17,17 @@ switch(symbol) {
     break;
     case '/':
     alert(number1 / number2);
-}*/
+}
 
 /*Task number 2. Couldn´t solve this one. I don´t understand why we have
 this type of advanced mathematics in the beginning of this course. It doesn't make
 any sense to me. It just make me feel bad and ruins my self confidence regarding the course.*/
 
-let number = +prompt('Write any even number');
-let sum = 0; 
+let number = +prompt('Write any even number'); 
 if (number % 2 === 0) {
+    let sum = 0;
     for (let i = 1; i <= number; i++) {
-    sum = i * sum; 
+    sum = sum + i; 
     }
     console.log(sum);
 }
@@ -37,7 +37,7 @@ if (number % 2 === 0) {
 
 // Task number 3. 
 
-/*let number = prompt('Write any positive integer');  
+let number = prompt('Write any positive integer');  
 
 if (number >= 0) {
     for (let i = 1; i <= number; i++) {
@@ -45,14 +45,14 @@ if (number >= 0) {
     } 
 } else {
     document.write('<p>error</p>');
-}*/
+}
 
 
 
 // Task number 4.
 
-/*let answer;
+let answer;
 do {
   answer = prompt('What is 2 + 2 * 2?');
 } while (answer != 6);
-alert('You answered correctly!');*/
+alert('You answered correctly!');
